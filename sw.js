ec077da7eb8408ff9fb573c88dd58933322edb71
iispolicorotursi <antonio.fittipaldi@pitagorapolicoro.eu>
2025-10-01 20:53:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" }).catch(() => {
+      return new Response("Offline", {
+        status: 503,
+        statusText: "Offline"
+      });
+    })
+  );
 });
